b175ca66adf8639d64c8d9afdddfebd7229f0e89
suryanshgupta133@gmail.com <suryanshgupta133@gmail.com>
2023-07-19 22:28:27 +0530

    Add post request for users

--- backend/db.js
+++ backend/db.js
@@ -11,8 +11,7 @@
 // module.exports = connectToMongo;
 
 const mongoose = require("mongoose");
-const mongoURI =
-  "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const mongoURI = "mongodb://127.0.0.1:27017";
 
 const connectToMongo = async () => {
   try {
@@ -25,5 +24,13 @@ const connectToMongo = async () => {
     console.error("Error connecting to MongoDB:", error);
   }
 };
+
+// const connectToMongo = () => {
+//   mongoose.connect(mongoURI, () => {
+//     console.log("Connected to Mongo Successfully");
+//   });
+// };
+
+module.exports = connectToMongo;
 
 module.exports = connectToMongo;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,31 @@
+// const express = require("express");
+// const router = express.Router();
+// const User = require("../models/User");
+
+// // Create a User using: POST "/api/auth/". Doesn't require Auth
+// router.post("/", (req, res) => {
+//   console.log(req.body);
+//   const user = User(req.body);
+//   user.save();
+//   res.send(req.body);
+// });
+
+// module.exports = router;
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  const obj = {
-    a: "thsa",
-    number: 34,
-  };
-  res.json(obj);
+// Create a User using: POST "/api/auth/". Doesn't require Auth
+router.post("/", async (req, res) => {
+  try {
+    console.log(req.body);
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({ error: "Failed to create user" });
+  }
 });
 
 module.exports = router;
